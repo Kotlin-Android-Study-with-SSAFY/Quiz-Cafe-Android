6e6cab8d585be205668365df141a0399c08f331b
parksangyun <park48569803@gmail.com>
2025-04-21 17:35:52 +0900

    fix : pr 커멘트 반영2

--- app/src/main/java/com/android/quizcafe/main/navigation/routes/AuthRoutes.kt
+++ app/src/main/java/com/android/quizcafe/main/navigation/routes/AuthRoutes.kt
@@ -4,8 +4,8 @@ import com.android.quizcafe.main.navigation.NavRoute
 
 sealed interface AuthRoute : NavRoute {
     object Graph    : MainRoute { override val route = "auth" }
-    object Login  : AuthRoute { override val route = "auth/login" }
-    object Signup : AuthRoute { override val route = "auth/signup" }
+    object Login  : AuthRoute { override val route = "${Graph.route}/login" }
+    object Signup : AuthRoute { override val route = "${Graph.route}/signup" }
 
     companion object {
         val startDestination = Login.route
--- app/src/main/java/com/android/quizcafe/main/navigation/routes/MainRoute.kt
+++ app/src/main/java/com/android/quizcafe/main/navigation/routes/MainRoute.kt
@@ -4,9 +4,9 @@ import com.android.quizcafe.main.navigation.NavRoute
 
 sealed interface MainRoute : NavRoute {
     object Graph    : MainRoute { override val route = "main" }
-    object Quiz     : MainRoute { override val route = "main/quiz" }
-    object MyPage   : MainRoute { override val route = "main/mypage" }
-    object Workbook : MainRoute { override val route = "main/workbook" }
+    object Quiz     : MainRoute { override val route = "${Graph.route}/quiz" }
+    object MyPage   : MainRoute { override val route = "${Graph.route}/mypage" }
+    object Workbook : MainRoute { override val route = "${Graph.route}/workbook" }
 
     companion object {
         val startDestination = Quiz.route
