0fec23a0d11590a3df485e37d0e3dcf4d62a397c
ootr47 <ootr45@gmail.com>
2025-05-11 21:44:52 +0900

    feat: 카테고리 선택 ui 구현

---
+++ app/src/main/java/com/android/quizcafe/feature/categorypicker/CategoryPickerContent.kt
@@ -0,0 +1,93 @@
+package com.android.quizcafe.feature.categorypicker
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.android.quizcafe.R
+import com.android.quizcafe.core.designsystem.theme.QuizCafeTheme
+import com.android.quizcafe.core.designsystem.theme.quizCafeTypography
+
+@Composable
+fun CategoryCardList(categories: List<Category>) {
+    LazyColumn {
+        items(categories) { category ->
+            CategoryCard(category = category)
+        }
+    }
+}
+
+@Composable
+fun CategoryCard(category: Category) {
+    Spacer(Modifier.height(8.dp))
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White),
+        onClick = {
+            // TODO: 문제집 화면으로 이동
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = category.group,
+                color = Color.Gray,
+                style = quizCafeTypography().bodyLarge
+            )
+            Spacer(Modifier.height(8.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = category.name,
+                    style = quizCafeTypography().titleMedium
+                )
+
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                    contentDescription = stringResource(R.string.go),
+                    tint = Color.Black,
+                    modifier = Modifier.height(24.dp)
+                )
+            }
+        }
+    }
+    Spacer(Modifier.height(8.dp))
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryCardPreview() {
+    QuizCafeTheme {
+        CategoryCardList(listOf(Category("그룹", "카테고리 이름"), Category("그룹", "카테고리 이름")))
+    }
+}
