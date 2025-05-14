88b387e68b0d3f105fa004a0ce4a4c3704c3e94b
inseong <superohinsung@naver.com>
2025-05-14 15:45:11 +0900

    feat: QuizViewModel을 BaseViewModel 연결

--- app/src/main/java/com/android/quizcafe/feature/main/quiz/QuizContract.kt
+++ app/src/main/java/com/android/quizcafe/feature/main/quiz/QuizContract.kt
@@ -1,6 +1,7 @@
 package com.android.quizcafe.feature.main.quiz
 
 import androidx.compose.ui.graphics.Color
+import com.android.quizcafe.core.ui.base.BaseContract
 
 data class ModeItem(
     val titleResId: Int,
@@ -15,14 +16,15 @@ data class QuizHistory(
     val totalProblems: Int
 )
 
+// TODO: viewState, Intent, Effect 전부다 재설정 필요.
 data class QuizViewState(
     val historyList: List<QuizHistory> = emptyList()
-)
+) : BaseContract.ViewState
 
-sealed class QuizIntent {
+sealed class QuizIntent : BaseContract.ViewIntent {
     data class LoadHistory(val histories: List<QuizHistory>) : QuizIntent()
 }
 
-sealed class QuizEffect {
+sealed class QuizEffect : BaseContract.ViewEffect {
     data class ShowErrorDialog(val message: String) : QuizEffect()
 }
--- app/src/main/java/com/android/quizcafe/feature/main/quiz/QuizViewModel.kt
+++ app/src/main/java/com/android/quizcafe/feature/main/quiz/QuizViewModel.kt
@@ -1,24 +1,19 @@
 package com.android.quizcafe.feature.main.quiz
 
-import androidx.lifecycle.ViewModel
+import com.android.quizcafe.core.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 @HiltViewModel
-class QuizViewModel @Inject constructor() : ViewModel() {
+class QuizViewModel @Inject constructor() : BaseViewModel<QuizViewState, QuizIntent, QuizEffect>(
+    initialState = QuizViewState()
+) {
 
-    private val _state = MutableStateFlow(QuizViewState())
-    val state: StateFlow<QuizViewState> = _state.asStateFlow()
-
-    private val _effect = MutableSharedFlow<QuizEffect>()
-    val effect: SharedFlow<QuizEffect> = _effect.asSharedFlow()
-
-    fun onIntent(intent: QuizIntent) {
-        _state.value = reduce(_state.value, intent)
+    override suspend fun handleIntent(intent: QuizIntent) {
+        TODO("Not yet implemented")
     }
 
-    private fun reduce(state: QuizViewState, intent: QuizIntent): QuizViewState {
+    override fun reduce(state: QuizViewState, intent: QuizIntent): QuizViewState {
         return when (intent) {
             is QuizIntent.LoadHistory -> state.copy(historyList = intent.histories)
         }
