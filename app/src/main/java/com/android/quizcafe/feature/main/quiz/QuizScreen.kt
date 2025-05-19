af65d50b2d0d78fc926f0c255d9998d2af8f808a
inseong <superohinsung@naver.com>
2025-05-19 16:13:15 +0900

    fix: Refactor Code

--- app/src/main/java/com/android/quizcafe/feature/main/quiz/QuizScreen.kt
+++ app/src/main/java/com/android/quizcafe/feature/main/quiz/QuizScreen.kt
@@ -1,6 +1,5 @@
 package com.android.quizcafe.feature.main.quiz
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -9,32 +8,25 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.android.quizcafe.R
 import com.android.quizcafe.core.designsystem.theme.QuizCafeTheme
-import com.android.quizcafe.core.ui.TitleWithUnderLine
 
 @Composable
 fun QuizScreen(
     state: QuizViewState,
-    sendIntent: (QuizIntent) -> Unit
+    onIntent: (QuizIntent) -> Unit
 ) {
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .padding(horizontal = 16.dp)
+            .padding(horizontal = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        TitleWithUnderLine(stringResource(R.string.tab_title_quiz))
-        Spacer(Modifier.height(12.dp))
-        LazyColumn(
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            item { QuizHistorySection(historyList = state.historyList) }
-            item { Spacer(modifier = Modifier.height(16.dp)) }
-            item { QuizModeSection { sendIntent(QuizIntent.ClickQuizCard("")) } }
-        }
+        item { QuizHistorySection(historyList = state.historyList) }
+        item { Spacer(modifier = Modifier.height(4.dp)) }
+        item { QuizModeSection() }
+        item { Spacer(modifier = Modifier.height(6.dp)) }
     }
 }
 
@@ -50,7 +42,7 @@ fun QuizScreenPreview() {
                     QuizHistory("04/01", "재용이의 안드로이드", 19, 20)
                 )
             ),
-            sendIntent = {}
+            onIntent = {}
         )
     }
 }
