c95609cb1bc4692c79dad62e199248821d741b28
park99999 <sungung1026@naver.com>
2025-06-10 14:35:32 +0900

    feat : quizSolveViewModel에서 필요한 usecase 정의

--- app/src/main/java/com/android/quizcafe/feature/quiz/solve/viewmodel/QuizSolveViewModel.kt
+++ app/src/main/java/com/android/quizcafe/feature/quiz/solve/viewmodel/QuizSolveViewModel.kt
@@ -9,7 +9,14 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class QuizSolveViewModel @Inject constructor() : BaseViewModel<QuizSolveUiState, QuizSolveIntent, QuizSolveEffect>(
+class QuizSolveViewModel @Inject constructor(
+    /*
+    * 1. 문제집 정보 가져오는 UseCase -> 총 퀴즈수랑 각각의 퀴즈 id 가져올 수 있게
+    * 2. 퀴즈id를 통한 문제 정보(객관식은 보기까지) 가져오는 UseCase
+    * 3. 문제 채점 UseCase(아직 API가 없는듯?)
+    * 4. 다음문제로 넘어갈때 로컬?혹은 서버로 현재 문제 state 저장하는 UseCase
+    * */
+) : BaseViewModel<QuizSolveUiState, QuizSolveIntent, QuizSolveEffect>(
     initialState = QuizSolveUiState()
 ) {
     init {
