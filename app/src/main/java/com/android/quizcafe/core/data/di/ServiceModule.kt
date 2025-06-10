a79e1d6af1e28ec618e5bd3df64d7a1979b0a5a0
Jae Woong <ys05059@gmail.com>
2025-06-10 14:59:42 +0900

    feat : 퀴즈북 관련 remote, service 추가

--- app/src/main/java/com/android/quizcafe/core/data/di/ServiceModule.kt
+++ app/src/main/java/com/android/quizcafe/core/data/di/ServiceModule.kt
@@ -2,6 +2,7 @@ package com.android.quizcafe.core.data.di
 
 import com.android.quizcafe.core.data.remote.service.AuthService
 import com.android.quizcafe.core.data.remote.service.QuizBookService
+import com.android.quizcafe.core.data.remote.service.QuizBookSolvingService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -25,4 +26,10 @@ object ServiceModule {
     fun provideQuizBookService(
         @Named("token") retrofit: Retrofit
     ): QuizBookService = retrofit.create(QuizBookService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideQuizBookSolvingService(
+        @Named("token") retrofit: Retrofit
+    ): QuizBookSolvingService = retrofit.create(QuizBookSolvingService::class.java)
 }
