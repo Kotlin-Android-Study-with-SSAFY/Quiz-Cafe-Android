685976fb02ab7ebc2ef6715cdf9e4fb525b77f03
oh in seong <58154638+ois0886@users.noreply.github.com>
2025-06-12 01:20:16 +0900

    feat: ViewModel, Contract 구현 및 Data Service 구현

--- app/src/main/java/com/android/quizcafe/core/data/di/ServiceModule.kt
+++ app/src/main/java/com/android/quizcafe/core/data/di/ServiceModule.kt
@@ -2,6 +2,7 @@ package com.android.quizcafe.core.data.di
 
 import com.android.quizcafe.core.data.remote.service.AuthService
 import com.android.quizcafe.core.data.remote.service.QuizBookService
+import com.android.quizcafe.core.data.remote.service.UserService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -25,4 +26,10 @@ object ServiceModule {
     fun provideQuizBookService(
         @Named("token") retrofit: Retrofit
     ): QuizBookService = retrofit.create(QuizBookService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideUserService(
+        @Named("token") retrofit: Retrofit
+    ) : UserService = retrofit.create(UserService::java.)
 }
---
+++ app/src/main/java/com/android/quizcafe/feature/main/mypage/MyPageContract.kt
@@ -0,0 +1,38 @@
+package com.android.quizcafe.feature.main.mypage
+
+import com.android.quizcafe.core.ui.base.BaseContract
+
+data class MyPageViewState(
+    val userName: String = "",
+    val solvedCount: Int = 0,
+    val myQuizSetCount: Int = 0,
+    val quizSolvingRecord: Map<String, Int> = emptyMap(),
+    val joinDateStr: String = "",
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null,
+) : BaseContract.ViewState
+
+sealed class MyPageIntent : BaseContract.ViewIntent {
+    data object LoadUserInfo : MyPageIntent()
+    data class ClickStats(val solvedCount: Int) : MyPageIntent()
+    data object ClickAlarm : MyPageIntent()
+    data object ClickChangePw : MyPageIntent()
+    data object ClickMyQuizSet : MyPageIntent()
+
+    data class SuccessLoadUserInfo(
+        val userName: String,
+        val solvedCount: Int,
+        val myQuizSetCount: Int,
+        val quizSolvingRecord: Map<String, Int>,
+        val joinDateStr: String
+    ) : MyPageIntent()
+    data class FailLoadUserInfo(val errorMessage: String) : MyPageIntent()
+}
+
+sealed class MyPageEffect : BaseContract.ViewEffect {
+    data object NavigateToStats : MyPageEffect()
+    data object NavigateToAlarm : MyPageEffect()
+    data object NavigateToChangePw : MyPageEffect()
+    data object NavigateToMyQuizSet : MyPageEffect()
+    data class ShowError(val message: String) : MyPageEffect()
+}
