e83e969a6542ec850ca096f601a9216c31aa38c7
park99999 <sungung1026@naver.com>
2025-06-24 10:48:24 +0900

    feat : 한문제씩 풀기 변수

--- app/src/main/java/com/android/quizcafe/feature/quizbookdetail/QuizBookDetailScreen.kt
+++ app/src/main/java/com/android/quizcafe/feature/quizbookdetail/QuizBookDetailScreen.kt
@@ -18,6 +18,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -37,7 +42,7 @@ fun QuizBookDetailScreen(
     sendIntent: (QuizBookDetailIntent) -> Unit
 ) {
     val quizBookDetail = state.quizBookDetail
-
+    var isChecked by remember { mutableStateOf(false) }
 
     Scaffold(
         bottomBar = {
